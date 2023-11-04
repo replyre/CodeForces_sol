d999acc519d2af1ee75cdefd87095286cd4e5b7c
Rahul Gupta <121796450+replyre@users.noreply.github.com>
2023-11-04 22:14:35 +0530

    Update BeautifulMatrix.cpp

--- BeautifulMatrix.cpp
+++ BeautifulMatrix.cpp
@@ -1,17 +1,7 @@
 #include <bits/stdc++.h>
 #include <iostream>
 using namespace std;
-typedef long long ll;
-typedef vector<int> vi;
-typedef pair<int, int> pi;
-#define F first
-#define S second
-#define PB push_back
-#define MP make_pair
-#define forn(i, n) for (int i = 0; i < int(n); i++)
 
-// freopen('input.txt', 'r', stdin);
-// freopen('output.txt', 'w', stdout);
 int main()
 {
     ios_base::sync_with_stdio(0);
