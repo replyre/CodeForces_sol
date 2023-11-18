fb3a41a8a3c6cc3ebf2635dffe167d1397fa580e
Rahul Gupta <121796450+replyre@users.noreply.github.com>
2023-11-18 14:30:03 +0530

    Update Dragons.cpp

--- Dragons.cpp
+++ Dragons.cpp
@@ -1,23 +1,11 @@
 #include <bits/stdc++.h>
 #include <iostream>
 using namespace std;
-typedef long long ll;
-typedef vector<int> vi;
-typedef pair<int, int> pi;
-#define F first
-#define S second
-#define PB push_back
-#define MP make_pair
-#define forn(i, n) for (int i = 0; i < int(n); i++)
-
-// freopen('input.txt', 'r', stdin);
-// freopen('output.txt', 'w', stdout);
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    // int _t;cin>>_t; while(_t--)
     int s, n;
     cin >> s >> n;
     vector<pair<int, int>> dragons;
