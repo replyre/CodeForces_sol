e5da08c414a605fbb915a7171281e212c11c8ef8
Rahul Gupta <121796450+replyre@users.noreply.github.com>
2023-12-02 23:13:58 +0530

    Update EvenOdds.cpp

--- EvenOdds.cpp
+++ EvenOdds.cpp
@@ -2,10 +2,6 @@
 using namespace std;
 int main()
 {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    // int_t;cin>>_t; while(_t--)
     long long n, m;
     cin >> n >> m;
 
